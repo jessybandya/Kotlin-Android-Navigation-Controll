60957aa751d7a45099e70c63407a129c27fd26aa
Uwimana Jessy Bandya <jessy.bandya5@gmail.com>
2022-06-26 00:46:20 +0300

    done with toolbar break.

--- app/src/main/java/com/example/navigation_controll/MainActivity.kt
+++ app/src/main/java/com/example/navigation_controll/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.navigation_controll
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.fragment)
+
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
